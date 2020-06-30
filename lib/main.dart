3f05f205ef261445a62c1c93f22ea1c210d691e7
Aditya Singh <samant04aditya@gmail.com>
2020-07-01 00:34:48 +0530

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      title: 'Corona Tracker',
+      home: HomePage(),
+    );
+  }
+}
