1f84e813be4e26add6fd32de956299d2fc33e209
Aditya Singh <samant04aditya@gmail.com>
2020-07-08 22:45:43 +0530

    mediaQuery size

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 const kTextColor = Color(0xFF1E2432);
 const kTextMediumColor = Color(0xFF53627C);
 const kTextLightColor = Color(0xFFACB1C0);
-const kPrimaryColor = Color(0xFF0D8E53);
+const kPrimaryColor = Colors.blue;
 const kBackgroundColor = Color(0xFFFCFCFC);
 const kInactiveChartColor = Color(0xFFEAECEF);
 
-AppBar buildAppBar() {
-  return AppBar(
-    backgroundColor: kPrimaryColor.withOpacity(.03),
-    elevation: 0,
-    leading: IconButton(
-      icon: SvgPicture.asset("assets/icons/menu.svg"),
-      onPressed: () {},
-    ),
-    actions: <Widget>[
-      IconButton(
-        icon: SvgPicture.asset("assets/icons/search.svg"),
-        onPressed: () {},
-      )
-    ],
-    title: Text("Covid Tracker"),
-  );
+class SizeConfig {
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double blockSizeHorizontal;
+  static double blockSizeVertical;
+  static double _safeAreaHorizontal;
+  static double _safeAreaVertical;
+  static double safeBlockHorizontal;
+  static double safeBlockVertical;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    blockSizeHorizontal = screenWidth / 100;
+    blockSizeVertical = screenHeight / 100;
+    _safeAreaHorizontal =
+        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+    _safeAreaVertical =
+        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
+    safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
+  }
 }
