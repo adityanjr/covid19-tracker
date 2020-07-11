a1b372cf4c044aebb352e8b2a837ab335b63caa3
Aditya Singh <samant04aditya@gmail.com>
2020-07-11 20:09:09 +0530

    BloC: Dynamic Theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/MainDrawer.dart';
 import 'screens/HomePage.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDark;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          DrawerScreen(),
-          HomePage(),
-        ],
-      ),
+    return StreamBuilder(
+      stream: bloc.darkThemeEnabled,
+      initialData: false,
+      builder: (context, snapshot) => MaterialApp(
+          theme: snapshot.data ? ThemeData.dark() : ThemeData.light(),
+          home: HomePage(snapshot.data)),
     );
   }
 }
+
+class Bloc {
+  // ignore: close_sinks
+  final _themeController = StreamController<bool>();
+  get changeTheme => _themeController.sink.add;
+  get darkThemeEnabled => _themeController.stream;
+}
+
+final bloc = Bloc();
