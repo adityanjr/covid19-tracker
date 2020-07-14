f35a5dd6d0fc2e2fa8e14c8680156b83dcb1f55b
Aditya Singh <samant04aditya@gmail.com>
2020-07-14 23:22:50 +0530

    Gesture in InfoCard

--- lib/widgets/infoCard.dart
+++ lib/widgets/infoCard.dart
@@ -4,6 +4,7 @@ import '../constants.dart';
 import 'lineCharts.dart';
 
 class InfoCard extends StatelessWidget {
+  final Function press;
   final String title;
   final int effectedNum;
   final Color color;
@@ -12,66 +13,71 @@ class InfoCard extends StatelessWidget {
     this.title,
     this.effectedNum,
     this.color,
+    this.press,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        return Container(
-          width: constraints.maxWidth / 2 - 10,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(SizeConfig.safeBlockHorizontal * 3),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      alignment: Alignment.center,
-                      height: SizeConfig.safeBlockHorizontal * 8,
-                      width: SizeConfig.safeBlockVertical * 5,
-                      decoration: BoxDecoration(
-                          color: color.withOpacity(.2), shape: BoxShape.circle),
-                      child: SvgPicture.asset(
-                        "assets/icons/running.svg",
-                        height: SizeConfig.safeBlockHorizontal * 5,
-                        color: color,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: SizeConfig.safeBlockHorizontal * 2),
-                      child: Text(
-                        title,
-                        style: TextStyle(
-                          fontFamily: "Kayak",
-                          fontSize: SizeConfig.safeBlockHorizontal * 4,
+        return GestureDetector(
+          onTap: press,
+          child: Container(
+            width: constraints.maxWidth / 2 - 10,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(SizeConfig.safeBlockHorizontal * 3),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.center,
+                        height: SizeConfig.safeBlockHorizontal * 8,
+                        width: SizeConfig.safeBlockVertical * 5,
+                        decoration: BoxDecoration(
+                            color: color.withOpacity(.2),
+                            shape: BoxShape.circle),
+                        child: SvgPicture.asset(
+                          "assets/icons/running.svg",
+                          height: SizeConfig.safeBlockHorizontal * 5,
+                          color: color,
                         ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: SizeConfig.safeBlockHorizontal * 2),
+                        child: Text(
+                          title,
+                          style: TextStyle(
+                            fontFamily: "Kayak",
+                            fontSize: SizeConfig.safeBlockHorizontal * 4,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.only(
+                            left: SizeConfig.safeBlockHorizontal * 3),
+                        child: Text(
+                          "$effectedNum\n",
+                          style: Theme.of(context).textTheme.headline6.copyWith(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Titillium",
+                              fontSize: SizeConfig.safeBlockHorizontal * 7),
+                        )),
+                    Expanded(
+                      child: LineReportChart(
+                        chartColor: color,
                       ),
                     )
                   ],
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Padding(
-                      padding: EdgeInsets.only(
-                          left: SizeConfig.safeBlockHorizontal * 3),
-                      child: Text(
-                        "$effectedNum\n",
-                        style: Theme.of(context).textTheme.headline6.copyWith(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Titillium",
-                            fontSize: SizeConfig.safeBlockHorizontal * 7),
-                      )),
-                  Expanded(
-                    child: LineReportChart(
-                      chartColor: color,
-                    ),
-                  )
-                ],
-              )
-            ],
+                )
+              ],
+            ),
           ),
         );
       },
