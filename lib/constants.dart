6fea04a743ea5e7711a1f84bf11f78e776bb08ce
Aditya Singh <samant04aditya@gmail.com>
2020-07-31 02:49:34 +0530

    animated routes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:animations/animations.dart";
 
 const kTextColor = Color(0xFF1E2432);
 const kTextMediumColor = Color(0xFF53627C);
@@ -30,5 +31,55 @@ class SizeConfig {
         _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
     safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
     safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
+  }
+}
+
+double xMargin(double x) {
+  return SizeConfig.safeBlockHorizontal * x;
+}
+
+double yMargin(double x) {
+  return SizeConfig.safeBlockVertical * x;
+}
+
+class AnimatedRoutes {
+  static Route<T> fadeThrough<T>(page, [double duration = 1000]) {
+    return PageRouteBuilder<T>(
+      transitionDuration: Duration(milliseconds: (duration * 1000).round()),
+      pageBuilder: (context, animation, secondaryAnimation) => page(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeThroughTransition(
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+            child: child);
+      },
+    );
+  }
+
+  static Route<T> fadeScale<T>(page, [double duration = 1000]) {
+    return PageRouteBuilder<T>(
+      transitionDuration: Duration(milliseconds: (duration * 1000).round()),
+      pageBuilder: (context, animation, secondaryAnimation) => page(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeScaleTransition(animation: animation, child: child);
+      },
+    );
+  }
+
+  static Route<T> sharedAxis<T>(page,
+      [SharedAxisTransitionType type = SharedAxisTransitionType.scaled,
+      double duration = 1000]) {
+    return PageRouteBuilder<T>(
+      transitionDuration: Duration(milliseconds: (duration * 1000).round()),
+      pageBuilder: (context, animation, secondaryAnimation) => page(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return SharedAxisTransition(
+          child: child,
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          transitionType: type,
+        );
+      },
+    );
   }
 }
